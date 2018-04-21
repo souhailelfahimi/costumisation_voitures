deeee91c7c174b2b46893c323c874a3ce09fe7ca
xXx-BlackAngel-xXx <xXx-BlackAngel-xXx@BlackAngel>
2018-04-21 15:37:17 +0100

    +inscription

---
+++ src/e_benz/E_BENZ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e_benz;
+
+/**
+ *
+ * @author xXx-BlackAngel-xXx
+ */
+public class E_BENZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
